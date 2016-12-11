1c96402e582693ac6ca459c7d775331dff13bbd7
neerajmangal <neerajmangal@outlook.com>
2016-12-11 21:06:04 +0530

    Removed API access Token

---
+++ src/main/java/com/nmangal/rally/application/RallyConnector.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.nmangal.rally.application;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.rallydev.rest.RallyRestApi;
+
+/**
+ * @author nmangal
+ *
+ */
+public class RallyConnector {
+
+	private final static String RallyUrl = "https://rally1.rallydev.com";
+	private static RallyRestApi rallyRestApiConnector = null;
+	private static final String RALLY_ACCESS_API_KEY="provide Rally API access token here";
+
+	/**
+	 * 
+	 */
+	private RallyConnector() {
+		
+	}
+
+	public static RallyRestApi getRallyConnector(){
+		if (rallyRestApiConnector != null) {
+			return rallyRestApiConnector;
+		}
+		
+        try {
+			rallyRestApiConnector = new RallyRestApi(new URI(RallyUrl),
+					RALLY_ACCESS_API_KEY);
+			rallyRestApiConnector.setApplicationName("Rally To Spark Integration");
+			rallyRestApiConnector.setApplicationVersion("1.0");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rallyRestApiConnector;
+	}
+}
